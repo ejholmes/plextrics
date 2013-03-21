596ab5ddbbd6ece687e1317a445c1b6710a5d285
Eric J. Holmes <eric@ejholmes.net>
2013-03-20 20:39:40 -0700

    cleanup.

--- lib/plextrics/services/stathat.rb
+++ lib/plextrics/services/stathat.rb
@@ -2,17 +2,19 @@ require 'stathat'
 
 module Plextrics
   class Service::Stathat < Service
+    attr_reader :ezkey
+
     def initialize(params)
       super
       @ezkey = params[:ezkey]
     end
 
     def count(metric)
-      ::StatHat::API.ez_post_count(metric, @ezkey, 1)
+      ::StatHat::API.ez_post_count(metric, ezkey, 1)
     end
 
     def gauge(metric, value)
-      ::StatHat::API.ez_post_value(metric, @ezkey, value)
+      ::StatHat::API.ez_post_value(metric, ezkey, value)
     end
   end
 end
